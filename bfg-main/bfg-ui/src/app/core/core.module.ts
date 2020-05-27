4378e6b84e797a9b996cf0a08aee4740c7205f4d
Ihar-Pinchuk1 <ihar.pinchuk1@ibm.com>
2020-05-27 18:58:40 +0300

    SIRP-1542 UI Implement auth service
    Implement service to log in with login and password, store recieved user info, save info to LocalStorage and login automaticallu with token.
    All interceptor to add auth token to requests

--- bfg-main/bfg-ui/src/app/core/core.module.ts
+++ bfg-main/bfg-ui/src/app/core/core.module.ts
@@ -4,6 +4,8 @@ import { LayoutComponent } from './components/layout/layout.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { MenuComponent } from './components/menu/menu.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './auth/auth.interceptor';
 
 
 
@@ -19,6 +21,11 @@ import { MenuComponent } from './components/menu/menu.component';
   ],
   exports: [
     LayoutComponent
-  ]
+  ],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  }]
 })
 export class CoreModule { }

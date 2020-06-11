18caa104483fd07d716c8177e296f17ab31b2588
Ihar-Pinchuk1 <ihar.pinchuk1@ibm.com>
2020-06-11 16:47:08 +0300

    Add Swagger documentation and bat build script

---
+++ bfg-main/bfg-api/build_and_deploy.bat
@@ -0,0 +1,7 @@
+call mvn liberty:stop
+if not "%ERRORLEVEL%" == "0" exit /b
+call mvn clean install
+if not "%ERRORLEVEL%" == "0" exit /b
+call mvn liberty:start
+if not "%ERRORLEVEL%" == "0" exit /b
+call mvn liberty:deploy

1a114d0703a1ec03cddb15aca04f4a122a91d722
Viktoryia Hurevich <Viktoryia.Hurevich@ibm.com>
2020-06-26 14:49:48 +0300

    SIRP-1245 Added PreAuthorize for CertificateController

--- bfg-main/bfg-api/src/main/java/com/ibm/sterling/bfg/app/controller/CertificateController.java
+++ bfg-main/bfg-api/src/main/java/com/ibm/sterling/bfg/app/controller/CertificateController.java
@@ -2,12 +2,13 @@ package com.ibm.sterling.bfg.app.controller;
 
 import com.ibm.sterling.bfg.app.model.TrustedCertificateDetails;
 import com.ibm.sterling.bfg.app.service.CertificateValidationService;
+import com.ibm.sterling.bfg.app.utils.ListToPageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.naming.InvalidNameException;
@@ -16,6 +17,7 @@ import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.util.ArrayList;
 
 import static org.springframework.http.ResponseEntity.ok;
 
@@ -26,7 +28,15 @@ public class CertificateController {
     @Autowired
     private CertificateValidationService certificateValidationService;
 
+    @GetMapping
+    @PreAuthorize("hasAuthority('FB_UI_TRUSTED_CERTS')")
+    public Page<?> getCertificates(@RequestParam(value = "size", defaultValue = "10", required = false) Integer size,
+                                   @RequestParam(value = "page", defaultValue = "0", required = false) Integer page) {
+        return ListToPageConverter.convertListToPage(new ArrayList<String>(), PageRequest.of(page, size));
+    }
+
     @PostMapping("/upload")
+    @PreAuthorize("hasAuthority('FB_UI_TRUSTED_CERTS_NEW')")
     public ResponseEntity<TrustedCertificateDetails> uploadFile(@RequestParam("file") MultipartFile certificate)
             throws CertificateException, IOException, InvalidNameException, NoSuchAlgorithmException {
         CertificateFactory factory = CertificateFactory.getInstance("X.509");
--- bfg-main/bfg-api/src/main/java/com/ibm/sterling/bfg/app/model/exception/CertificateErrorCode.java
+++ bfg-main/bfg-api/src/main/java/com/ibm/sterling/bfg/app/model/exception/CertificateErrorCode.java
@@ -5,7 +5,8 @@ public enum CertificateErrorCode implements ErrorCode {
     FAIL("CER_FAIL_CODE", "CER_FAIL_MSG", "CER_FAIL_STATUS"),
     RuntimeException("CER_RTE_CODE", "CER_RTE_MSG", "CER_RTE_STATUS"),
     NullPointerException("CER_NPE_CODE", "CER_NPE_MSG", "CER_NPE_STATUS"),
-    CertificateNotFoundException("CER_NOT_FOUND_CODE", "CER_NOT_FOUND_MSG", "CER_NOT_FOUND_STATUS");
+    CertificateNotFoundException("CER_NOT_FOUND_CODE", "CER_NOT_FOUND_MSG", "CER_NOT_FOUND_STATUS"),
+    AccessDeniedException("CER_ACCESS_DENIED_CODE", "CER_ACCESS_DENIED_MSG", "CER_ACCESS_DENIED_STATUS");
 
     CertificateErrorCode(String value, String msg, String status) {
         this.val = value;

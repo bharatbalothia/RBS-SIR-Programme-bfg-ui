83f7cd34bb64c54d2c4f83c8a46c860227a744fc
Viktoryia Hurevich <Viktoryia.Hurevich@ibm.com>
2020-08-05 09:19:01 +0300

    SIRP-1256 Added search for files by criteria

--- bfg-main/bfg-api/src/main/java/com/ibm/sterling/bfg/app/model/file/FileSearchCriteria.java
+++ bfg-main/bfg-api/src/main/java/com/ibm/sterling/bfg/app/model/file/FileSearchCriteria.java
@@ -1,5 +1,128 @@
 package com.ibm.sterling.bfg.app.model.file;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
 public class FileSearchCriteria {
+    private String service;
+    private String reference;
+    private Long messageid;
+    private Integer entityid;
+    private Boolean override;
+    private Boolean outbound;
+    private Integer wfid;
+    private String filename;
+    private Integer start;
+    private Integer rows;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime from;
+    private String type;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime to;
+
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public Long getMessageid() {
+        return messageid;
+    }
+
+    public void setMessageid(Long messageid) {
+        this.messageid = messageid;
+    }
+
+    public Integer getEntityid() {
+        return entityid;
+    }
+
+    public void setEntityid(Integer entityid) {
+        this.entityid = entityid;
+    }
+
+    public Boolean getOverride() {
+        return override;
+    }
+
+    public void setOverride(Boolean override) {
+        this.override = override;
+    }
+
+    public Boolean getOutbound() {
+        return outbound;
+    }
+
+    public void setOutbound(Boolean outbound) {
+        this.outbound = outbound;
+    }
+
+    public Integer getWfid() {
+        return wfid;
+    }
+
+    public void setWfid(Integer wfid) {
+        this.wfid = wfid;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public Integer getStart() {
+        return start;
+    }
+
+    public void setStart(Integer start) {
+        this.start = start;
+    }
+
+    public Integer getRows() {
+        return rows;
+    }
+
+    public void setRows(Integer rows) {
+        this.rows = rows;
+    }
+
+    public LocalDateTime getFrom() {
+        return from;
+    }
+
+    public void setFrom(LocalDateTime from) {
+        this.from = from;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getTo() {
+        return to;
+    }
+
+    public void setTo(LocalDateTime to) {
+        this.to = to;
+    }
 
 }
--- bfg-main/bfg-api/src/main/java/com/ibm/sterling/bfg/app/service/FileSearchService.java
+++ bfg-main/bfg-api/src/main/java/com/ibm/sterling/bfg/app/service/FileSearchService.java
@@ -1,9 +1,12 @@
 package com.ibm.sterling.bfg.app.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ibm.sterling.bfg.app.model.file.File;
+import com.ibm.sterling.bfg.app.model.file.FileSearchCriteria;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -11,12 +14,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
-import java.util.Base64;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+
+import java.util.*;
 
 import static com.ibm.sterling.bfg.app.utils.RestTemplatesConstants.HEADER_PREFIX;
 
@@ -32,9 +35,10 @@ public class FileSearchService {
     @Value("${file.password}")
     private String password;
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    private ObjectMapper objectMapper;
 
-    public Page<File> getFilesList(Integer page, Integer size) throws JsonProcessingException {
+    public Page<File> getFilesList(FileSearchCriteria fileSearchCriteria, Integer page, Integer size) throws JsonProcessingException {
         HttpHeaders headers = new HttpHeaders();
         String userCredentials = userName + ":" + password;
         headers.set(HttpHeaders.AUTHORIZATION,
@@ -42,18 +46,21 @@ public class FileSearchService {
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
 
+        Map<String, String> fileSearchCriteriaMap = objectMapper.convertValue(fileSearchCriteria, new TypeReference<Map<String, String>>() {
+        });
+        fileSearchCriteriaMap.values().removeIf(Objects::isNull);
+        MultiValueMap<String, String> fileSearchCriteriaMultiValueMap = new LinkedMultiValueMap<>();
+        fileSearchCriteriaMap.forEach(fileSearchCriteriaMultiValueMap::add);
+
         UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(fileSearchUrl)
                 .queryParam("start", page)
-                .queryParam("rows", size);
-
-        HttpEntity request = new HttpEntity<>(headers);
-        ResponseEntity<String> response =
-                new RestTemplate().exchange(
-                        uriBuilder.toUriString(),
-                        HttpMethod.GET,
-                        request,
-                        String.class
-                );
+                .queryParam("rows", size)
+                .queryParams(fileSearchCriteriaMultiValueMap);
+        ResponseEntity<String> response = new RestTemplate().exchange(
+                uriBuilder.build().toString(),
+                HttpMethod.GET,
+                new HttpEntity<>(headers),
+                String.class);
         JsonNode root = objectMapper.readTree(Objects.requireNonNull(response.getBody()));
         JsonNode totalRows = root.get("totalRows");
         JsonNode results = root.get("results");
@@ -63,6 +70,5 @@ public class FileSearchService {
         Pageable pageable = PageRequest.of(page, size);
         return new PageImpl<>(fileList, pageable, totalElements);
     }
-
 
 }

a1d20aed4a9a77e97296d0f9a29b9e3d49e374a3
Ihar-Pinchuk1 <ihar.pinchuk1@ibm.com>
2020-08-31 17:19:31 +0300

    fix SIRP-1926 UI cleanup and format new code

--- bfg-main/bfg-ui/src/app/shared/directives/disable-control.directive.ts
+++ bfg-main/bfg-ui/src/app/shared/directives/disable-control.directive.ts
@@ -1,18 +1,17 @@
 import { Directive, Input } from '@angular/core';
 import { NgControl } from '@angular/forms';
-import { MatSelect } from '@angular/material/select';
 
 @Directive({
   selector: '[appDisableControl]'
 })
 export class DisableControlDirective {
 
-  @Input() set appDisableControl( condition: boolean ) {
-    if (condition && this.ngControl.control){
+  @Input() set appDisableControl(condition: boolean) {
+    if (condition && this.ngControl.control) {
       this.ngControl.control.disable();
     }
   }
 
-  constructor( private ngControl: NgControl ) { }
+  constructor(private ngControl: NgControl) { }
 
 }

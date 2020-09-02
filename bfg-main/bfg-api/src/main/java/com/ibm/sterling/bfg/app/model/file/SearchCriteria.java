f481b965746485c1ed284dad7843004aa9c9ee24
Maria-Suhoparova <54477906+Maria-Suhoparova@users.noreply.github.com>
2020-09-02 15:00:14 +0300

    SIRP-1266 Added: inheritance for search criteria

--- bfg-main/bfg-api/src/main/java/com/ibm/sterling/bfg/app/model/file/SearchCriteria.java
+++ bfg-main/bfg-api/src/main/java/com/ibm/sterling/bfg/app/model/file/SearchCriteria.java
@@ -1,4 +1,75 @@
 package com.ibm.sterling.bfg.app.model.file;
 
-public interface SearchCriteria {
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.Nulls;
+
+public class SearchCriteria {
+    private String type;
+    private String service;
+    private String reference;
+    private Boolean outbound;
+    private Integer wfid;
+    @JsonAlias("page")
+    @JsonSetter(nulls = Nulls.SKIP)
+    private Integer start = 0;
+    @JsonAlias("size")
+    @JsonSetter(nulls = Nulls.SKIP)
+    private Integer rows = 10;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public Boolean getOutbound() {
+        return outbound;
+    }
+
+    public void setOutbound(Boolean outbound) {
+        this.outbound = outbound;
+    }
+
+    public Integer getWfid() {
+        return wfid;
+    }
+
+    public void setWfid(Integer wfid) {
+        this.wfid = wfid;
+    }
+
+    public Integer getStart() {
+        return start;
+    }
+
+    public void setStart(Integer start) {
+        this.start = start;
+    }
+
+    public Integer getRows() {
+        return rows;
+    }
+
+    public void setRows(Integer rows) {
+        this.rows = rows;
+    }
 }
--- bfg-main/bfg-api/src/main/java/com/ibm/sterling/bfg/app/model/file/TransactionSearchCriteria.java
+++ bfg-main/bfg-api/src/main/java/com/ibm/sterling/bfg/app/model/file/TransactionSearchCriteria.java
@@ -6,38 +6,20 @@ import com.ibm.sterling.bfg.app.model.validation.DateValid;
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 @JsonInclude(NON_NULL)
-public class TransactionSearchCriteria implements SearchCriteria{
-    private String type;
+public class TransactionSearchCriteria extends SearchCriteria{
     private String entity;
-    private String service;
-    private String reference;
     private String direction;
-    private Boolean outbound;
-    private Integer wfid;
     private String paymentbic;
-    private String transactionid;
+    @JsonAlias("transactionid")
+    private String transactionId;
     @DateValid(pattern = "yyyy-MM-dd'T'HH:mm:ss", message = "Please match the requested format for from")
     @JsonProperty("settlement-from")
-    @JsonAlias("settlementFrom")
+    @JsonAlias("from")
     private String settlementFrom;
     @DateValid(pattern = "yyyy-MM-dd'T'HH:mm:ss", message = "Please match the requested format for to")
     @JsonProperty("settlement-to")
-    @JsonAlias("settlementTo")
+    @JsonAlias("to")
     private String settlementTo;
-    @JsonAlias("page")
-    @JsonSetter(nulls = Nulls.SKIP)
-    private Integer start = 0;
-    @JsonAlias("size")
-    @JsonSetter(nulls = Nulls.SKIP)
-    private Integer rows = 10;
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
 
     public String getEntity() {
         return entity;
@@ -45,22 +27,6 @@ public class TransactionSearchCriteria implements SearchCriteria{
 
     public void setEntity(String entity) {
         this.entity = entity;
-    }
-
-    public String getService() {
-        return service;
-    }
-
-    public void setService(String service) {
-        this.service = service;
-    }
-
-    public String getReference() {
-        return reference;
-    }
-
-    public void setReference(String reference) {
-        this.reference = reference;
     }
 
     public String getDirection() {
@@ -71,22 +37,6 @@ public class TransactionSearchCriteria implements SearchCriteria{
         this.direction = direction;
     }
 
-    public Boolean getOutbound() {
-        return outbound;
-    }
-
-    public void setOutbound(Boolean outbound) {
-        this.outbound = outbound;
-    }
-
-    public Integer getWfid() {
-        return wfid;
-    }
-
-    public void setWfid(Integer wfid) {
-        this.wfid = wfid;
-    }
-
     public String getPaymentbic() {
         return paymentbic;
     }
@@ -95,12 +45,12 @@ public class TransactionSearchCriteria implements SearchCriteria{
         this.paymentbic = paymentbic;
     }
 
-    public String getTransactionid() {
-        return transactionid;
+    public String getTransactionId() {
+        return transactionId;
     }
 
-    public void setTransactionid(String transactionid) {
-        this.transactionid = transactionid;
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
     }
 
     public String getSettlementFrom() {
@@ -117,21 +67,5 @@ public class TransactionSearchCriteria implements SearchCriteria{
 
     public void setSettlementTo(String settlementTo) {
         this.settlementTo = settlementTo;
-    }
-
-    public Integer getStart() {
-        return start;
-    }
-
-    public void setStart(Integer start) {
-        this.start = start;
-    }
-
-    public Integer getRows() {
-        return rows;
-    }
-
-    public void setRows(Integer rows) {
-        this.rows = rows;
     }
 }

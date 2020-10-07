67e1298df0fa2bc9ce30654e241b5a58add4a65f
Viktoryia Hurevich <Viktoryia.Hurevich@ibm.com>
2020-10-07 12:40:15 +0300

    Added getUserAuthorities, getAuthoritiesByKey to PropertyService,

--- bfg-main/bfg-api/src/main/java/com/ibm/sterling/bfg/app/service/PermissionsService.java
+++ bfg-main/bfg-api/src/main/java/com/ibm/sterling/bfg/app/service/PermissionsService.java
@@ -11,6 +11,7 @@ import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+
 import java.util.*;
 import java.util.function.BiFunction;
 import java.util.function.Predicate;
@@ -34,6 +35,12 @@ public class PermissionsService {
     @Value("${permissions.password}")
     private String password;
 
+    @Value("${property.useraccountGroupsKey}")
+    private String userAccountGroupsKey;
+
+    @Value("${property.useraccountPermissionsKey}")
+    private String userAccountPermissionsKey;
+
     @Autowired
     private ObjectMapper objectMapper;
 
@@ -56,11 +63,11 @@ public class PermissionsService {
                 String.class
         );
         JsonNode root = objectMapper.readTree(Objects.requireNonNull(responseEntity.getBody()));
+        List<Map<String, String>> authorityList = Optional.ofNullable(objectMapper.convertValue(root, List.class)).orElseGet(ArrayList::new);
 
-        List<Map<String, String>> authorityList = objectMapper.convertValue(root, List.class);
-
-        List<String> userAccountPermissions = propertyService.getUserAccountPermissions();
-        List<String> userAccountGroups = propertyService.getUserAccountGroups();
+        Map<String, List<String>> userAuthorities = propertyService.getUserAuthorities();
+        List<String> userAccountGroups = userAuthorities.get(userAccountGroupsKey);
+        List<String> userAccountPermissions = userAuthorities.get(userAccountPermissionsKey);
         Map<String, Set<String>> permissions = permissionsService.getPermissionsOfBFGUIGroups(userAccountGroups, userAccountPermissions);
 
         return authorityList.stream()
@@ -68,37 +75,26 @@ public class PermissionsService {
                             Set<String> permissionSet = getAuthoritySet.apply(authorities, "permissions");
                             Set<String> groupSet = getAuthoritySet.apply(authorities, "groups");
                             if (!groupSet.isEmpty()) {
-                                groupSet.removeIf(isNotNeededPermsPredicate(userAccountGroups));
+                                groupSet.removeIf(isNotNeededAuthorityPredicate(userAccountGroups));
                                 groupSet.forEach(group -> permissionSet.addAll(permissions.get(group)));
                             }
-                            permissionSet.removeIf(isNotNeededPermsPredicate(userAccountPermissions));
+                            permissionSet.removeIf(isNotNeededAuthorityPredicate(userAccountPermissions));
                             return permissionSet.stream();
                         }
                 ).collect(Collectors.toList());
     }
 
     @Cacheable(cacheNames = CACHE_PERMISSIONS)
-    public Map<String, Set<String>> getPermissionsOfBFGUIGroups(List<String> listBFGUIGroups, List<String> listBFGUIPermissions)
-            {
+    public Map<String, Set<String>> getPermissionsOfBFGUIGroups(List<String> listOfBFGUIGroups, List<String> listOfBFGUIPermissions) {
         Map<String, Set<String>> permissions = new HashMap<>();
-        listBFGUIGroups.forEach(group -> {
+        listOfBFGUIGroups.forEach(group -> {
             try {
-                permissions.put(group, getPermissionsOfGroup(group, false, listBFGUIPermissions));
+                permissions.put(group, getPermissionsOfGroup(group, false, listOfBFGUIPermissions));
             } catch (JsonProcessingException e) {
                 e.printStackTrace();
             }
         });
         return permissions;
-    }
-
-    private HttpHeaders getHttpHeaders() {
-        HttpHeaders headers = new HttpHeaders();
-        String userCredentials = userName + ":" + password;
-        headers.set(HttpHeaders.AUTHORIZATION,
-                HEADER_PREFIX + Base64.getEncoder().encodeToString(userCredentials.getBytes()));
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        return headers;
     }
 
     public Set<String> getPermissionsOfGroup(String group, boolean isChild, List<String> listBFGUIPermissions) throws JsonProcessingException {
@@ -107,17 +103,13 @@ public class PermissionsService {
                 HttpMethod.GET,
                 new HttpEntity<>(getHttpHeaders()),
                 String.class);
-
         JsonNode root = objectMapper.readTree(Objects.requireNonNull(response.getBody()));
-
-        List<Map<String, String>> authorityList = objectMapper.convertValue(root, List.class);
+        List<Map<String, String>> authorityList = Optional.ofNullable(objectMapper.convertValue(root, List.class)).orElseGet(ArrayList::new);
 
         return authorityList.stream()
                 .flatMap(authorities -> {
                             Set<String> permissionSet = getAuthoritySet.apply(authorities, "permissions");
-
                             if (!isChild) {
-
                                 Set<String> subgroups = getAuthoritySet.apply(authorities, "subgroups");
                                 subgroups.forEach(subgroup -> {
                                     try {
@@ -129,21 +121,31 @@ public class PermissionsService {
 
                                 });
                             }
-                            permissionSet.removeIf(isNotNeededPermsPredicate(listBFGUIPermissions));
+                            permissionSet.removeIf(isNotNeededAuthorityPredicate(listBFGUIPermissions));
                             return permissionSet.stream();
                         }
                 ).collect(Collectors.toSet());
     }
 
-    private Predicate<String> isNotNeededPermsPredicate(List<String> userAccountPermissions) {
-        return perm -> !userAccountPermissions.contains(perm);
+    private Predicate<String> isNotNeededAuthorityPredicate(List<String> userAccountAuthorities) {
+        return authority -> !userAccountAuthorities.contains(authority);
     }
 
-    private BiFunction<Map<String, String>, String ,Set<String>> getAuthoritySet = (authorityMap, name) -> {
-        List<Map<String, String>> authorityList =
-                objectMapper.convertValue(authorityMap.get(name), List.class);
+    private BiFunction<Map<String, String>, String, Set<String>> getAuthoritySet = (authorityMap, name) -> {
+        List<Map<String, String>> authorityList = objectMapper.convertValue(authorityMap.get(name), List.class);
         return Optional.ofNullable(authorityList)
                 .map(authList -> authorityList.stream().map(authority -> authority.get("name")).collect(Collectors.toSet()))
-                .orElse(new HashSet<String>());
+                .orElse(new HashSet<>());
     };
+
+    private HttpHeaders getHttpHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        String userCredentials = userName + ":" + password;
+        headers.set(HttpHeaders.AUTHORIZATION,
+                HEADER_PREFIX + Base64.getEncoder().encodeToString(userCredentials.getBytes()));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        return headers;
+    }
+
 }

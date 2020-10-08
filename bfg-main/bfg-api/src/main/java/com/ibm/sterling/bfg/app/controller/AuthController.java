52c254b9f693cd4f23947bd01be3e1e63da7cc2c
Maryia-Sukhaparava <maryia.sukhaparava@ibm.com>
2020-10-08 13:30:09 +0300

    SIRP-1986 Removed import

--- bfg-main/bfg-api/src/main/java/com/ibm/sterling/bfg/app/controller/AuthController.java
+++ bfg-main/bfg-api/src/main/java/com/ibm/sterling/bfg/app/controller/AuthController.java
@@ -1,10 +1,9 @@
 package com.ibm.sterling.bfg.app.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.ibm.sterling.bfg.app.model.security.Login;
+import com.ibm.sterling.bfg.app.model.security.LoginRequest;
 import com.ibm.sterling.bfg.app.model.security.LoginSsoRequest;
 import com.ibm.sterling.bfg.app.security.JwtTokenGenerator;
-import com.ibm.sterling.bfg.app.model.security.LoginRequest;
 import com.ibm.sterling.bfg.app.service.CredentialsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Collections;
 
 import static org.springframework.http.ResponseEntity.ok;

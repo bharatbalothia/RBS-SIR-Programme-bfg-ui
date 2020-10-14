f9a412ba9966ba998a32aeedfd9fae8a6bc6c3e5
Ihar-Pinchuk1 <ihar.pinchuk1@ibm.com>
2020-10-14 18:37:44 +0300

    fix-SIRP-2094 Add logger

--- bfg-main/bfg-api/src/main/java/com/ibm/sterling/bfg/app/controller/MasterErrorController.java
+++ bfg-main/bfg-api/src/main/java/com/ibm/sterling/bfg/app/controller/MasterErrorController.java
@@ -1,15 +1,26 @@
 package com.ibm.sterling.bfg.app.controller;
 
+import com.ibm.sterling.bfg.app.exception.AuthExceptionHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MasterErrorController implements ErrorController {
+    private static final Logger LOG = LogManager.getLogger(AuthExceptionHandler.class);
     @RequestMapping("/error")
-    public String handleError() {
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        LOG.info("Request to /error handled in MasterErrorController.");
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+            LOG.info("Error status code is %s", statusCode);
+        }
         return "index.html";
     }
 

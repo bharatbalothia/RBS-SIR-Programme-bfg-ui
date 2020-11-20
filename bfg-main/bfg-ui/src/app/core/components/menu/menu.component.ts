6087e4f8dfd739af8e36dd7434460cdb032e1537
Yauheni Shapaval <Yauheni.Shapaval@ibm.com>
2020-11-20 13:51:27 +0300

    fix-SIRP-2106 fixing router reloading

--- bfg-main/bfg-ui/src/app/core/components/menu/menu.component.ts
+++ bfg-main/bfg-ui/src/app/core/components/menu/menu.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NestedTreeControl } from '@angular/cdk/tree';
 import { MatTreeNestedDataSource } from '@angular/material/tree';
 import { MenuNode, MENU_DATA } from './menu-nodes';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -14,11 +15,18 @@ export class MenuComponent implements OnInit {
   treeControl = new NestedTreeControl<MenuNode>(node => node.children);
   dataSource = new MatTreeNestedDataSource<MenuNode>();
 
-  constructor() {
+  constructor(
+    private router: Router
+  ) {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+
     this.dataSource.data = MENU_DATA;
-   }
+  }
 
   ngOnInit(): void {
+    this.treeControl.expand(this.dataSource.data
+      .find(el => this.hasChild(null, el) && el.children.find(child => child.route === this.router.url.split('/')[1])));
   }
 
   hasChild = (_: number, node: MenuNode) => !!node.children && node.children.length > 0;

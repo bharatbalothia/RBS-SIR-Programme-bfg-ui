7742174543e51295876e0e076661b852c7f62964
Viktoryia Hurevich <Viktoryia.Hurevich@ibm.com>
2021-01-12 14:11:41 +0300

    SIRP-2526 Added tokenSecretAuthData to server.xml, commented security.jwt.tokenSigningKey

--- bfg-main/bfg-api/src/main/java/com/ibm/sterling/bfg/app/config/DynamicPropertiesListener.java
+++ bfg-main/bfg-api/src/main/java/com/ibm/sterling/bfg/app/config/DynamicPropertiesListener.java
@@ -22,11 +22,13 @@ public class DynamicPropertiesListener implements ApplicationListener<Applicatio
         try {
             AuthData jdbcAuthData = AuthDataProvider.getAuthData("jdbcAuthData");
             AuthData apiAuthData = AuthDataProvider.getAuthData("apiAuthData");
+            AuthData tokenSecretAuthData = AuthDataProvider.getAuthData("tokenSecretAuthData");
             Properties props = new Properties();
             props.put("spring.datasource.username", passwordDecode(jdbcAuthData.getUserName()));
             props.put("spring.datasource.password", passwordDecode(new String(jdbcAuthData.getPassword())));
             props.put("api.userName", passwordDecode(apiAuthData.getUserName()));
             props.put("api.password", passwordDecode(new String(apiAuthData.getPassword())));
+            props.put("security.jwt.tokenSigningKey", passwordDecode(new String(tokenSecretAuthData.getPassword())));
             environment.getPropertySources().addLast(new PropertiesPropertySource("dynamicProperties", props));
         } catch (LoginException e) {
             LOG.error("LoginException in onApplicationEvent of DynamicPropertiesListener: " + e.getMessage());

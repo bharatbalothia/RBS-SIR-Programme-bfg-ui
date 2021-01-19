e453b6e6b8ff3b499f7e85d12e771eb1e803a92d
UKalatsei <Uladzislau.Kalatsei@ibm.com>
2021-01-19 16:11:54 +0300

    Added password confirmation dialog; Fixed style issue with auto wrap in notification bar

--- bfg-main/bfg-ui/src/app/shared/components/transmit-dialog/transmit-dialog.component.ts
+++ bfg-main/bfg-ui/src/app/shared/components/transmit-dialog/transmit-dialog.component.ts
@@ -6,6 +6,7 @@ import { ENTITY_TRANSMIT_FILE_TYPE } from '../../models/entity/entity-constants'
 import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 import { ConfirmDialogConfig } from '../confirm-dialog/confirm-dialog-config.model';
 import { Entity } from '../../models/entity/entity.model';
+import { PasswordConfirmationDialogComponent } from '../password-confirmation-dialog/password-confirmation-dialog.component';
 
 @Component({
   selector: 'app-transmit-dialog',
@@ -22,11 +23,12 @@ export class TransmitDialogComponent {
   fileType: string = ENTITY_TRANSMIT_FILE_TYPE.ICF;
 
   displayName: (fieldName: string) => string;
-  transmitAction: (id: string, type: string) => any;
+  transmitAction: (id: string, type: string, password: string) => any;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: DetailsDialogData,
     private dialog: MatDialogRef<TransmitDialogComponent>,
+    private passwordConfirmationDialog: MatDialog,
     private confirmationDialog: MatDialog,
   ) {
     this.data.yesCaption = this.data.yesCaption || 'Close';
@@ -36,22 +38,28 @@ export class TransmitDialogComponent {
   }
 
   transmit() {
-    this.isLoading = true;
-    this.transmitAction(this.entity.entityId, this.fileType)
-      .subscribe(() => {
-        this.isLoading = false;
-        this.dialog.afterClosed().subscribe(() => {
-          this.confirmationDialog.open(ConfirmDialogComponent, new ConfirmDialogConfig({
-            title: `Entity Transmit Now`,
-            text: `The Transmit Now function was successfully started for the  ${this.entity.entity} entity`,
-            shouldHideYesCaption: true,
-            noCaption: 'Return'
-          }));
-        });
-        this.dialog.close();
-      },
-        error => this.isLoading = false
-      );
+    this.passwordConfirmationDialog.open(PasswordConfirmationDialogComponent, {
+      width: '250px'
+    }).afterClosed().subscribe(password => {
+      if (password) {
+        this.isLoading = true;
+        this.transmitAction(this.entity.entityId, this.fileType, password)
+          .subscribe(() => {
+            this.isLoading = false;
+            this.dialog.afterClosed().subscribe(() => {
+              this.confirmationDialog.open(ConfirmDialogComponent, new ConfirmDialogConfig({
+                title: `Entity Transmit Now`,
+                text: `The Transmit Now function was successfully started for the  ${this.entity.entity} entity`,
+                shouldHideYesCaption: true,
+                noCaption: 'Return'
+              }));
+            });
+            this.dialog.close();
+          },
+            error => this.isLoading = false
+          );
+      }
+    });
   }
 
 }

23e1723ff1ae7002c6c06ffbe0fb2e981fd9a602
Maryia-Sukhaparava <maryia.sukhaparava@ibm.com>
2021-02-08 13:46:48 +0300

    SIRP-2448 Set version to variable

---
+++ bfg-main/bfg-api/src/main/java/com/ibm/sterling/bfg/app/controller/VersionController.java
@@ -0,0 +1,26 @@
+package com.ibm.sterling.bfg.app.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.ibm.sterling.bfg.app.model.statistics.StatisticalData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+@RestController
+@RequestMapping("api/version")
+public class VersionController {
+
+    @Autowired
+    BuildProperties buildProperties;
+
+    @GetMapping
+    public ResponseEntity<String> getApplicationVersion() {
+        return ok(buildProperties.getVersion());
+    }
+
+}

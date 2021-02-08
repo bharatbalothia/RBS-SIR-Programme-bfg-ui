0d21b7c87b81517258bd93d5488babc30d02a251
Maryia-Sukhaparava <maryia.sukhaparava@ibm.com>
2021-02-08 18:56:45 +0300

    SIRP-2522 Added DML for soft delete the entities of non SCT/GPL types

---
+++ bfg-main/DML_ SOFT_DELETE.sql
@@ -0,0 +1,10 @@
+--------------------------------------------------------
+--  DDL for SCT_ENTITY
+--------------------------------------------------------
+
+  UPDATE "$$TBLUSER$$".SCT_ENTITY
+    SET ISDELETED = 1,
+	SERVICE = ('DEL_' || ENTITY_ID || '_' || SERVICE),
+	MAILBOXPATHOUT = ('DEL_' || ENTITY_ID || '_' || MAILBOXPATHOUT),
+	MQQUEUEOUT= ('DEL_' || ENTITY_ID || '_' || MQQUEUEOUT)
+    WHERE SERVICE IN ('SDD', 'ROI', 'TRD');

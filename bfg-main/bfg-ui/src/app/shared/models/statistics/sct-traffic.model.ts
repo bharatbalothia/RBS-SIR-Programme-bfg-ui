6ceb2d4c9f56d6ffdb4bc2af9d02ea3a71d0b176
Yauheni Shapaval <Yauheni.Shapaval@ibm.com>
2021-03-30 16:20:59 +0300

    SIRP-2853 fixing home page

--- bfg-main/bfg-ui/src/app/shared/models/statistics/sct-traffic.model.ts
+++ bfg-main/bfg-ui/src/app/shared/models/statistics/sct-traffic.model.ts
@@ -1,11 +1,17 @@
 export interface SCTTraffic {
-    SCT_FILES: DateTraffic;
-    SCT_TX: DateTraffic;
-    SCT_PAW: DateTraffic;
+    SCT_FILES_DAY: DateTraffic;
+    SCT_FILES_HOUR: DateTraffic;
+    SCT_FILES_WEEK: DateTraffic;
+    SCT_TX_DAY: DateTraffic;
+    SCT_TX_HOUR: DateTraffic;
+    SCT_TX_WEEK: DateTraffic;
+    SCT_PAW_DAY: DateTraffic;
+    SCT_PAW_HOUR: DateTraffic;
+    SCT_PAW_WEEK: DateTraffic;
 }
 
 interface DateTraffic {
-    HOUR: number;
-    WEEK: number;
-    DAY: number;
+    TOTAL: number;
+    OB?: number;
+    IB?: number;
 }

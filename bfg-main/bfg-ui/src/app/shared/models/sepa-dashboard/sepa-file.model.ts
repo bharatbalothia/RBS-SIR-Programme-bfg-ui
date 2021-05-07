5be4bb4564767e1ec8e7d1ee21a0b38678e5669b
Maryia-Sukhaparava <Maryia.Sukhaparava@ibm.com>
2021-05-07 18:19:37 +0300

    Revert "Merge pull request #286 from RBS-SIR-Programme/SIRP-2803-SEPA-Dashboard-Number-of-transactions-with-value"
    This reverts commit 9fa32438a41abcb4bf2d5a833908e05c179b55b4, reversing
    changes made to 66af4400c9102436e42bdbd9f8b89d3f27834b89.

--- bfg-main/bfg-ui/src/app/shared/models/sepa-dashboard/sepa-file.model.ts
+++
@@ -1,9 +0,0 @@
-export interface SEPAFile {
-    filename: string;
-    id: number;
-    direction: string;
-    docID: string;
-    settleAmountTotal: number;
-    transactionTotal: number;
-    type: string;
-}

71f4fd4cd1030bad9f7de62210f15170b4f12f38
Maryia-Sukhaparava <Maryia.Sukhaparava@ibm.com>
2021-08-27 11:47:07 +0300

    Update PropertyServiceTest.java

--- bfg-main/bfg-api/src/test/java/com/ibm/sterling/bfg/app/service/PropertyServiceTest.java
+++ bfg-main/bfg-api/src/test/java/com/ibm/sterling/bfg/app/service/PropertyServiceTest.java
@@ -1,7 +1,11 @@
 package com.ibm.sterling.bfg.app.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
 import com.ibm.sterling.bfg.app.service.entity.EntityService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,16 +13,23 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.*;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.spring.web.json.Json;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.*;
@@ -28,8 +39,13 @@ import static org.mockito.Mockito.when;
 //@ExtendWith(SpringExtension.class)
 //@EnableConfigurationProperties(value = PropertySettings.class)
 //@TestPropertySource("classpath:application-test.properties")
+//@TestPropertySource(properties = {
+//        "api.userName=userName", "api.password=password"})
+@ExtendWith(MockitoExtension.class)
+//@ContextConfiguration(classes = {PropertyServiceTest.MockConfig.class})
 class PropertyServiceTest {
     @InjectMocks
+    @Resource
     private PropertyService propertyService;
 
     @Mock
@@ -52,6 +68,12 @@ class PropertyServiceTest {
     private String password;
     private HttpHeaders headers;
 
+//    @Value("#{testProps['api.userName']}")
+//    private String userName;
+//
+//    @Value("#{testProps['api.password']}")
+//    private String password;
+
     private String gplList = "[\n" +
             "  {\n" +
             "    \"propertyKey\": \"gpl.ui.rtm.BPAYM.PM\",\n" +
@@ -72,8 +94,24 @@ class PropertyServiceTest {
         headers.setBasicAuth(userName, password);
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        MockitoAnnotations.initMocks(this);
+        ReflectionTestUtils.setField(propertyService, "userName", userName);
+        ReflectionTestUtils.setField(propertyService, "password", password);
+//        MockitoAnnotations.initMocks(this);
     }
+
+//    @Configuration
+//    public static class MockConfig{
+//        @Bean
+////        public Properties testProps(){
+////            Properties properties = new Properties();
+////            properties.setProperty("api.userName", "api.userName");
+////            properties.setProperty("api.password", "password");
+////            return properties;
+////        }
+//        PropertyService getPropertyService() {
+//            return new PropertyService();
+//        }
+//    }
 
     @Test
     void getInboundRequestType() throws JsonProcessingException {
@@ -100,18 +138,23 @@ class PropertyServiceTest {
         when(settings.getGplUrl()).thenReturn("http://gpl.com");
         ResponseEntity<String> responseEntity = new ResponseEntity<>(gplList, HttpStatus.OK);
         when(apiDetailsHandler.getHttpHeaders(userName, password)).thenReturn(headers);
-//        doReturn(responseEntity).when(restTemplate)
-//                .exchange(
-//                        "http://gpl.com", HttpMethod.GET,
-//                        new HttpEntity<>(apiDetailsHandler.getHttpHeaders(userName, password)),
-//                        String.class);
+        doReturn(responseEntity).when(restTemplate).
+            exchange(
+                anyString(), any(HttpMethod.class), any(HttpEntity.class), eq(String.class));
+        ObjectMapper mapper = new ObjectMapper();
+
+        JsonNode node = mapper.readTree(gplList);
+        when(objectMapper.readTree(anyString()))
+                .thenReturn(node);
+        when(objectMapper.convertValue(any(), eq(List.class)))
+                .thenReturn(mapper.convertValue(node, List.class));
 //        when(restTemplate.exchange(
 //                settings.getGplUrl(), HttpMethod.GET,
 //                new HttpEntity<>(apiDetailsHandler.getHttpHeaders(userName, password)),
 //                String.class)).thenReturn(responseEntity);
-                when(restTemplate.exchange(
-                anyString(), any(HttpMethod.class), any(HttpEntity.class), eq(String.class)))
-                .thenReturn(new ResponseEntity(gplList, HttpStatus.OK));
+//                when(restTemplate.exchange(
+//                anyString(), any(HttpMethod.class), any(HttpEntity.class), eq(String.class)))
+//                .thenReturn(new ResponseEntity<String>(gplList, HttpStatus.OK));
         Map<String, String> inboundRequestTypes = new HashMap<>();
         inboundRequestTypes.put("BPAYM.PM", "pacs.xxx.fisp.bpaym");
         inboundRequestTypes.put("DLINK.DL", "pacs.xxx.fisp.pospay");
